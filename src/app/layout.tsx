251761b9f19b167092f682baa70aa24f47b5a9b3
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2025-08-31 21:06:05 +0200

    Added googletagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -51,6 +52,20 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans bg-gray-50`}>
         {children}
+
+        {/* Google Analytics */}
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-8W8PKL59B2"
+            strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8W8PKL59B2');
+          `}
+        </Script>
       </body>
     </html>
   );
